a9b3289d184221b3d69d1d14937eaa72616fc985
Skip <skip@usecurling.com>
2026-04-15 15:15:59 +0000

    Fix login and remove authentication

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster } from '@/components/ui/toaster'
 import { Toaster as Sonner } from '@/components/ui/sonner'
 import { TooltipProvider } from '@/components/ui/tooltip'
 import { StoreProvider } from '@/stores/main'
+import { AuthProvider } from '@/hooks/use-auth'
 
 import Layout from './components/Layout'
 import Index from './pages/Index'
@@ -18,26 +19,28 @@ import Values from './pages/cadastros/Values'
 
 const App = () => (
   <BrowserRouter future={{ v7_startTransition: false, v7_relativeSplatPath: false }}>
-    <StoreProvider>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <Routes>
-          <Route path="/registrar-plantao" element={<PublicRegisterShift />} />
-          <Route element={<Layout />}>
-            <Route path="/" element={<Index />} />
-            <Route path="/aprovacoes" element={<Approvals />} />
-            <Route path="/pagamentos" element={<Payments />} />
-            <Route path="/cadastros/funcionarios" element={<Employees />} />
-            <Route path="/cadastros/aprovadores" element={<Approvers />} />
-            <Route path="/cadastros/hospedes" element={<Guests />} />
-            <Route path="/cadastros/motivos" element={<Reasons />} />
-            <Route path="/cadastros/valores" element={<Values />} />
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </TooltipProvider>
-    </StoreProvider>
+    <AuthProvider>
+      <StoreProvider>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+          <Routes>
+            <Route path="/registrar-plantao" element={<PublicRegisterShift />} />
+            <Route element={<Layout />}>
+              <Route path="/" element={<Index />} />
+              <Route path="/aprovacoes" element={<Approvals />} />
+              <Route path="/pagamentos" element={<Payments />} />
+              <Route path="/cadastros/funcionarios" element={<Employees />} />
+              <Route path="/cadastros/aprovadores" element={<Approvers />} />
+              <Route path="/cadastros/hospedes" element={<Guests />} />
+              <Route path="/cadastros/motivos" element={<Reasons />} />
+              <Route path="/cadastros/valores" element={<Values />} />
+            </Route>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </TooltipProvider>
+      </StoreProvider>
+    </AuthProvider>
   </BrowserRouter>
 )
 
